140eec31bc26f61b36457b3a3d7798e67c796431
stefanspringer1 <stefan.springer@icloud.com>
2021-08-28 16:27:37 +0200

    using SwiftXMLParser >= 0.1.57: using swift-tools-version:5.3 again

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser.git", from: "0.1.56"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser.git", from: "0.1.57"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
