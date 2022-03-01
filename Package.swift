7d847362460e2e05f730df4a9329719b021b9dbd
stefanspringer1 <stefan.springer@icloud.com>
2022-03-01 16:40:07 +0100

    using SwiftXMLParser >= 1.1.78

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "1.1.76"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "1.1.78"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
