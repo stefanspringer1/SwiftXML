4d1c349ab391760864c953a858879903e1d96f51
stefanspringer1 <stefan.springer@icloud.com>
2022-03-19 17:12:08 +0100

    using SwiftXMLParser >= 1.1.84

--- Sources/SwiftXML/Builder/XParseBuilder.swift
+++ Sources/SwiftXML/Builder/XParseBuilder.swift
@@ -25,7 +25,7 @@ public final class XParseBuilder: XEventHandler {
         
     }
     
-    public func xmlDeclaration(version: String, encoding: String?, standalone: String?, sourceRange _: SourceRange) {
+    public func xmlDeclaration(version: String, encoding: String?, standalone: String?, textRange _: XTextRange?, dataRange _: XDataRange?) {
         document.xmlVersion = version
         if let theEncoding = encoding {
             document.encoding = theEncoding
@@ -35,24 +35,24 @@ public final class XParseBuilder: XEventHandler {
         }
     }
     
-    public func documentTypeDeclarationStart(type: String, publicID: String?, systemID: String?, sourceRange _: SourceRange) {
+    public func documentTypeDeclarationStart(type: String, publicID: String?, systemID: String?, textRange _: XTextRange?, dataRange _: XDataRange?) {
         document.type = type
         document.publicID = publicID
         document.systemID = systemID
     }
     
-    public func documentTypeDeclarationEnd(sourceRange _: SourceRange) {
+    public func documentTypeDeclarationEnd(textRange _: XTextRange?, dataRange _: XDataRange?) {
         // -
     }
     
-    public func elementStart(name: String, attributes: [String:String]?, sourceRange _: SourceRange) {
+    public func elementStart(name: String, attributes: [String:String]?, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let element = XElement(name)
         currentBranch.add(element)
         element.setAttributes(attributes: attributes)
         currentBranch = element
     }
     
-    public func elementEnd(name: String, sourceRange _: SourceRange) {
+    public func elementEnd(name: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         if let parent = currentBranch._parent {
             currentBranch = parent
         }
@@ -61,67 +61,67 @@ public final class XParseBuilder: XEventHandler {
         }
     }
     
-    public func text(text: String, whitespace: WhitespaceIndicator, sourceRange _: SourceRange) {
+    public func text(text: String, whitespace: WhitespaceIndicator, textRange _: XTextRange?, dataRange _: XDataRange?) {
         currentBranch.add(XText(text, whitespace: whitespace))
     }
     
-    public func cdataSection(text: String, sourceRange _: SourceRange) {
+    public func cdataSection(text: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         currentBranch.add(XCDATASection(text: text))
     }
     
-    public func internalEntity(name: String, sourceRange _: SourceRange) {
+    public func internalEntity(name: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         currentBranch.add(XInternalEntity(name))
     }
     
-    public func externalEntity(name: String, sourceRange _: SourceRange) {
+    public func externalEntity(name: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         currentBranch.add(XExternalEntity(name))
     }
     
-    public func processingInstruction(target: String, data: String?, sourceRange _: SourceRange) {
+    public func processingInstruction(target: String, data: String?, textRange _: XTextRange?, dataRange _: XDataRange?) {
         currentBranch.add(XProcessingInstruction(target: target, data: data))
     }
     
-    public func comment(text: String, sourceRange _: SourceRange) {
+    public func comment(text: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         currentBranch.add(XComment(text: text))
     }
     
-    public func internalEntityDeclaration(name: String, value: String, sourceRange _: SourceRange) {
+    public func internalEntityDeclaration(name: String, value: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let decl = XInternalEntityDeclaration(name: name, value: value)
         document.internalEntityDeclarations[name] = decl
         document.declarationsInInternalSubset.append(decl)
     }
     
-    public func externalEntityDeclaration(name: String, publicID: String?, systemID: String, sourceRange _: SourceRange) {
+    public func externalEntityDeclaration(name: String, publicID: String?, systemID: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let decl = XExternalEntityDeclaration(name: name, publicID: publicID, systemID: systemID)
         document.externalEntityDeclarations[name] = decl
         document.declarationsInInternalSubset.append(decl)
     }
     
-    public func unparsedEntityDeclaration(name: String, publicID: String?, systemID: String, notation: String, sourceRange _: SourceRange) {
+    public func unparsedEntityDeclaration(name: String, publicID: String?, systemID: String, notation: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let decl = XUnparsedEntityDeclaration(name: name, publicID: publicID, systemID: systemID, notationName: notation)
         document.unparsedEntityDeclarations[name] = decl
         document.declarationsInInternalSubset.append(decl)
     }
     
-    public func notationDeclaration(name: String, publicID: String?, systemID: String?, sourceRange _: SourceRange) {
+    public func notationDeclaration(name: String, publicID: String?, systemID: String?, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let decl = XNotationDeclaration(name: name, publicID: publicID, systemID: systemID)
         document.notationDeclarations[name] = decl
         document.declarationsInInternalSubset.append(decl)
     }
     
-    public func elementDeclaration(name: String, literal: String, sourceRange _: SourceRange) {
+    public func elementDeclaration(name: String, literal: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let decl = XElementDeclaration(name: name, literal: literal)
         document.elementDeclarations[name] = decl
         document.declarationsInInternalSubset.append(decl)
     }
     
-    public func attributeListDeclaration(name: String, literal: String, sourceRange _: SourceRange) {
+    public func attributeListDeclaration(name: String, literal: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let decl = XAttributeListDeclaration(name: name, literal: literal)
         document.attributeListDeclarations[name] = decl
         document.declarationsInInternalSubset.append(decl)
     }
     
-    public func parameterEntityDeclaration(name: String, value: String, sourceRange _: SourceRange) {
+    public func parameterEntityDeclaration(name: String, value: String, textRange _: XTextRange?, dataRange _: XDataRange?) {
         let decl = XParameterEntityDeclaration(name: name, value: value)
         document.parameterEntityDeclarations[name] = decl
         document.declarationsInInternalSubset.append(decl)
