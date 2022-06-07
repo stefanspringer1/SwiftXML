18fa558e9dc76955ba563191e62e45f8d72967bf
Stefan Springer <springer@stefanspringer.com>
2022-06-07 09:28:46 +0200

    SwiftXMLParser >= 1.1.88 (bug fixing error when pasring JSON with Umlaute)

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "1.1.87"),
+        .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "1.1.88"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
