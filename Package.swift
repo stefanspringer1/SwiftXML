03fa864ed9ce12f62a695ddfc6dbcb5c0f871ebe
Stefan Springer <springer@stefanspringer.com>
2022-10-27 20:32:11 +0200

    using the AutoreleasepoolShim package

--- Package.swift
+++ Package.swift
@@ -14,13 +14,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/stefanspringer1/SwiftXMLParser", from: "1.1.113"),
+        .package(url: "https://github.com/stefanspringer1/AutoreleasepoolShim", from: "1.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftXML",
-            dependencies: ["SwiftXMLParser"]),
+            dependencies: [
+                "SwiftXMLParser",
+                "AutoreleasepoolShim",
+            ]
+        ),
         .testTarget(
             name: "SwiftXMLTests",
             dependencies: ["SwiftXML"]),
