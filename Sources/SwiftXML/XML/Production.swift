5b251e30df79d3f20a9ebbff58e2b09303ef359a
stefanspringer1 <stefan.springer@icloud.com>
2024-11-27 17:05:52 -0600

    reverted last change

--- Sources/SwiftXML/XML/Production.swift
+++ Sources/SwiftXML/XML/Production.swift
@@ -473,7 +473,6 @@ open class ActiveHTMLProduction: ActivePrettyPrintProduction {
             "\(fullHTMLPrefix)wbr"
         ]
         htmlStrictInlines = [
-            "\(fullHTMLPrefix)a",
             "\(fullHTMLPrefix)abbr",
             "\(fullHTMLPrefix)acronym",
             "\(fullHTMLPrefix)b",
